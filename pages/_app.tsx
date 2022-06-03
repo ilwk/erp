57c1ebf51d0362fc102070e64e0b7a8f143f61f1
io-craft <luwenjiechn@qq.com>
2022-06-03 21:53:38 +0800

    style: add nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
+import { useProgress } from "~/hooks/useProgress";
+import "../styles/nprogress.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useProgress();
   return <Component {...pageProps} />;
 }
 
