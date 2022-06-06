676714e1a738faf800ef28f49da8c3685551ed72
vg-land <luwenjiechn@qq.com>
2022-06-06 22:14:41 +0800

    wip: 修复打包问题

--- src/dashboard/DealsChart.tsx
+++ src/dashboard/DealsChart.tsx
@@ -5,8 +5,11 @@ import { Box, Link } from "@mui/material";
 import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
 import { useGetList } from "react-admin";
 import { startOfMonth, format } from "date-fns";
-
 import { Deal } from "../types";
+
+import { Group } from "@visx/group";
+import { Bar } from "@visx/shape";
+import { scaleLinear, scaleBand } from "@visx/scale";
 
 const multiplier = {
   opportunity: 0.2,
@@ -77,6 +80,36 @@ export const DealsChart = () => {
     { min: 0, max: 0 }
   );
 
+  console.log(months);
+
+  // We'll make some helpers to get at the data we want
+  const x = (d) => d.pending;
+  const y = (d) => +d.won / 100;
+  const width = 500;
+  const height = 500;
+
+  const margin = { top: 20, bottom: 20, left: 20, right: 20 };
+  // Then we'll create some bounds
+  const xMax = width - margin.left - margin.right;
+  const yMax = height - margin.top - margin.bottom;
+  // And then scale the graph by our data
+  const xScale = scaleBand({
+    range: [0, xMax],
+    round: true,
+    domain: months.map(x),
+    padding: 0.4,
+  });
+  const yScale = scaleLinear({
+    range: [yMax, 0],
+    round: true,
+    domain: [0, Math.max(...months.map(y))],
+  });
+
+  // Compose together the scale and accessor functions to get point functions
+  const compose = (scale, accessor) => (data) => scale(accessor(data));
+  const xPoint = compose(xScale, x);
+  const yPoint = compose(yScale, y);
+
   return (
     <>
       <Box display="flex" alignItems="center">
@@ -93,7 +126,24 @@ export const DealsChart = () => {
           Upcoming Deal Revenue
         </Link>
       </Box>
-      <Box height={500}></Box>
+      <Box height={height}>
+        <svg width={width} height={height}>
+          {months.map((d, i) => {
+            const barHeight = yMax - yPoint(d);
+            return (
+              <Group key={`bar-${i}`}>
+                <Bar
+                  x={xPoint(d)}
+                  y={yMax - barHeight}
+                  height={barHeight}
+                  width={xScale.bandwidth()}
+                  fill="#fc2e1c"
+                ></Bar>
+              </Group>
+            );
+          })}
+        </svg>
+      </Box>
     </>
   );
 };
