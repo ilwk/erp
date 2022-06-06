6bba809406094916f9cdd9b14ea6da6a064a4d6a
io-craft <luwenjiechn@qq.com>
2022-06-06 13:34:26 +0800

    chore: update title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,19 @@ import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import { useProgress } from "~/hooks/useProgress";
 import "../styles/nprogress.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   // 路由增加进度条
   useProgress();
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>ERP</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
